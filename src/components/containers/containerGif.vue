983bdc2f87da5c1510e8eb4de1ff0da03366ad49
Amancio302 <matheusamancio.dev@gmail.com>
2022-03-14 00:43:41 -0300

    container for images created

--- src/components/containers/containerGif.vue
+++ src/components/containers/containerGif.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="row justify-center">
-    <sui-image :src="src"
-      size="massive"
-      @load="onLoad"
-    />
+    <div 
+      :style="{
+        height,
+        width,
+        background: `url(${src})`,
+      }"
+      @mouseenter="toggleImageHover"
+      @mouseleave="toggleImageHover"
+    >
+      <div class="row justify-end" v-if="favorited && controllers.isImageHovered">
+        <div class="col cols-auto">
+          <sui-icon
+            name="star"
+            size="big"
+            class="star-icon"
+            :color="favorited ? 'yellow' : 'white'"
+            :outline="controllers.isIconHovered"
+            @mouseenter="toggleIconHover"
+            @mouseleave="toggleIconHover"
+            @click="onClick"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,16 +33,40 @@ export default {
   props: {
     src: {
       type: String
+    },
+    height: {
+      type: String
+    },
+    width: {
+      type: String
+    },
+    favorited: {
+      type: Boolean
     }
   },
+  data: () => ({
+    controllers: {
+      isImageHovered: false,
+      isIconHovered: false
+    }
+  }),
   methods: {
-    onLoad () {
-      console.log('image loaded...')
+    toggleImageHover () {
+      this.controllers.isImageHovered = !this.controllers.isImageHovered
+    },
+    toggleIconHover () {
+      this.controllers.isIconHovered = !this.controllers.isIconHovered
+    },
+    onClick (e) {
+      this.$emit('click', e)
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .star-icon {
+    cursor: pointer;
+    margin: 8px;
+  }
 </style>
