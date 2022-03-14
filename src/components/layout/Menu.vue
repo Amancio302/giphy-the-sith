57546a611c17eca771023a3738c83299933181df
Amancio302 <matheusamancio.dev@gmail.com>
2022-03-14 00:44:35 -0300

    Menu cog added

---
+++ src/components/layout/Menu.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="menu">
+    <div class="row justify-end">
+      <div class="col cols-auto config">
+        <div class="row">
+          <Icon
+            name="cog"
+            size="big"
+            inverted
+            @hover="toggleMenu"
+          />
+        </div>
+        <div class="row">
+          <div class="col cols-1">
+            <transition name="drop">
+              <div class="dropdown dropdown-menu" v-show="controllers.showMenu">
+                Ratinhooo
+              </div>
+            </transition>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Icon from '@/components/UI/Icon.vue'
+
+export default {
+  name: 'GTS-menu',
+  components: {
+    Icon
+  },
+  data : () => ({
+    controllers: {
+      showMenu: false
+    }
+  }),
+  methods: {
+    toggleMenu () {
+      this.controllers.showMenu = !this.controllers.showMenu
+    }
+  }
+}
+</script>
+
+<style>
+  .dropdown-menu {
+    /* margin-top: auto; */
+  }
+</style>
