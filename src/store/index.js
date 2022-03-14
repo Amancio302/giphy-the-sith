351c369c97ee6dfb4870eb784aac3871a156f5a0
Amancio302 <matheusamancio.dev@gmail.com>
2022-03-14 00:45:23 -0300

    gif store setup done

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    gifs: []
   },
   getters: {
+    gifs (state) {
+      let res
+      if (state.gifs.length === 0) res = JSON.parse(localStorage.getItem('gifs'))
+      else res = state.gifs
+      return res || []
+    }
   },
   mutations: {
+    addGif (state, payload) {
+      const gifs = Array.from(JSON.parse(localStorage.getItem('gifs')) || [])
+      if (gifs.filter(el => el.id === payload.id).length === 0) {
+        gifs.push(payload)
+        localStorage.setItem('gifs', JSON.stringify(gifs))
+        if (state.gifs.length === 0) {
+          state.gifs = gifs
+        } else {
+          state.gifs.push(payload)
+        }
+      }
+    }
   },
   actions: {
   },
