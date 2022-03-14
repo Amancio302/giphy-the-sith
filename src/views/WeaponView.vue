efc6fe60cae9e211eccca451ae432a3263bd5fd9
Amancio302 <matheusamancio.dev@gmail.com>
2022-03-14 17:34:54 -0300

    weapon view has some minor validations

--- src/views/WeaponView.vue
+++ src/views/WeaponView.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="row justify-center">
-    <div class="col cols-8 gif-selection">
-      <div class="row">
-        <div class="col">
-          <SelectorGif
-            v-model="gif"
-          />
-        </div>
-      </div>
-      <div class="row" v-if="!!gif">
-        <div class="col">
-          <h3 class="text-center selection-text">
-            Can it make him laught?
-          </h3>
-        </div>
-      </div>
-      <div class="row justify-center" v-if="!!gif">
-        <div class="col cols-4">
-          <sui-button color="red">
-            Não
-          </sui-button>
-        </div>
-        <div class="col cols-4">
-          <sui-button color="green" @click="onSuccessClick">
-            Sim
-          </sui-button>
-        </div>
+  <div id="weapon-view">
+    <div class="row">
+      <div class="col">
+        <SelectorGif
+          v-model="gif"
+        />
       </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <h3 class="text-center star-text my-2">
+          Adicionar ao arsenal?
+        </h3>
+      </div>
+    </div>
+    <div class="row justify-center">
+      <div class="col cols-4">
+        <sui-button :disabled="!gif" color="red" @click="onErrorClick">
+          Não
+        </sui-button>
+      </div>
+      <div class="col cols-4">
+        <sui-button :disabled="!gif" color="green" @click="onSuccessClick">
+          Sim
+        </sui-button>
+      </div>
+    </div>
+    <transition name="scale-fade">
+      <div v-show="controllers.showOverlay" class="overlay star-text already-added">
+        Esse gif já adicionado ao arsenal patrulheiro!
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -41,22 +44,32 @@ export default {
     SelectorGif
   },
   data: () => ({
-    gif: null
+    gif: null,
+    controllers: {
+      showOverlay: false
+    }
   }),
   methods: {
-    onSuccessClick () {
-      this.$store.commit('addGif', this.gif)
+    async onSuccessClick () {
+      const res = await this.$store.dispatch('addGif', this.gif)
+      console.log(res)
+      if (!res) {
+        this.controllers.showOverlay = true
+        setTimeout(() => {
+          this.controllers.showOverlay = false
+        }, 3000)
+      }
+    },
+    onErrorClick () {
+      this.gif = null
     }
   }
 }
 </script>
 
 <style scoped>
-  .gif-selection {
-    /* margin-top: 25vh; */
-  }
-  .selection-text {
-    color: #FFE81F;
-    font-family: StarJedi;
+  .already-added {
+    bottom: 0;
+    margin-bottom: 16px;
   }
 </style>
