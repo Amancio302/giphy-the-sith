e98e4c0ff2fbd4e1c511b141036ffe335b79317d
Amancio302 <matheusamancio.dev@gmail.com>
2022-03-17 09:45:33 -0300

    Added a modal component

---
+++ src/components/modals/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="modal">
+    <transition name="scale-fade">
+      <div class="overlay" :class="{ 'overlay-fixed': fixed }" v-if="value">
+        <div class="modal-container">
+          <div class="card pa-3 ma-3 baa-3 rounded-3" :class="{ 'dark-card': dark }">
+            <slot/>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GTS-modal-component',
+  props: {
+    value: {
+      type: Boolean
+    },
+    fixed: {
+      type: Boolean
+    },
+    dark: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/modals/UpdateGifModal.vue
+++ src/components/modals/UpdateGifModal.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="overlay update-gif-overlay" v-if="show">
-    <div class="card overlay-card">
-      <div class="ui grid">
-        <div class="centered row">
+  <div id="update-gif-modal">
+    <Modal :value="show" fixed dark>
+      <div class="ui grid update-gif-modal">
+        <div class="centered row align-center">
           <div class="one wide column"></div>
           <div class="thirteen wide column star-text">
             <div class="text-center">
@@ -11,12 +11,22 @@
           </div>
           <div class="one wide column">
             <div class="text-right">
-              <i class="big close icon yellow clickable" @click="onCloseClick"></i>
+              <i class="big close icon yellow clickable me-3" @click="onCloseClick"></i>
             </div>
           </div>
         </div>
         <div class="centered row">
-          <div class="ten wide column">
+          <div class="column" style="flex: 0">
+            <ContainerGif
+              :id="controllers.localdata.id"
+              :height="200"
+              link
+              show-attribution
+            />
+          </div>
+        </div>
+        <div class="centered row">
+          <div class="column">
             <div class="two ui buttons">
               <button class="ui button red" @click="onModalClick('delete')">
                 Deletar
@@ -27,63 +37,87 @@
             </div>
           </div>
         </div>
-        <div class="centered row">
-          <div class="ten wide column">
-            <transition name="fade">
-              <div id="update" v-if="controllers.action === 'update'">
+        <div class="one column centered row" :class="{ 'py-0 actions-inactive': !controllers.action }">
+          <div class="column">
+            <transition name="drop" mode="out-in">
+              <!-- Update -->
+              <div id="update" v-if="controllers.action === 'update'" key="update">
                 <div class="ui grid mb-4">
                   <div class="two column centered row">
-                    <div class="7 wide column">
+                    <div class="column">
                       <InputText
                         label="título"
                         v-model="controllers.localdata.title"
                       />
                     </div>
-                    <div class="7 wide column">
+                    <div class="column">
                       <InputText
                         label="url"
                         v-model="controllers.localdata.url"
                       />
                     </div>
                   </div>
-                </div>
-                <div class="two ui buttons">
-                  <button class="ui button basic red" @click="onModalClick(undefined)">
-                    Cancelar
-                  </button>
-                  <button class="ui button green" @click="onSubmitClick()">
-                    Enviar
-                  </button>
+                  <div class="centered row">
+                    <div class="column">
+                      <div class="two ui buttons">
+                        <button class="ui button basic red" @click="onCloseClick">
+                          Cancelar
+                        </button>
+                        <button class="ui button green" @click="onSubmitClick()">
+                          Enviar
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div id="delete" v-if="controllers.action === 'delete'">
-                <div class="star-text text-center mb-4">
-                  tem certeza disso patrulheiro? não há como voltar atrás depois disso
-                </div>
-                <div class="two ui buttons">
-                  <button class="ui button basic red" @click="onModalClick(undefined)">
-                    Repensar
-                  </button>
-                  <button class="ui button red" @click="onDeleteClick()">
-                    Deletar
-                  </button>
+              <!-- Delete -->
+              <div id="delete" v-if="controllers.action === 'delete'" key="delete">
+                <div class="ui grid mb-4">
+                  <div class="centered row">
+                    <div class="column">
+                      <div class="star-text text-center">
+                        tem certeza disso patrulheiro? não há como voltar atrás depois disso
+                      </div>
+                    </div>
+                  </div>
+                  <div class="centered row">
+                    <div class="column">
+                      <div class="two ui buttons">
+                        <div class="two ui buttons">
+                          <button class="ui button basic red" @click="onCloseClick">
+                            Repensar
+                          </button>
+                          <button class="ui button red" @click="onDeleteClick()">
+                            Deletar
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </transition>
           </div>
         </div>
       </div>
-    </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+
+import Modal from './Modal.vue'
+
+import ContainerGif from '@/components/containers/containerGif.vue'
 
 import InputText from '@/components/inputs/InputText.vue'
 
 export default {
   name: 'update-gif-modal',
   components: {
+    Modal,
+    ContainerGif,
     InputText
   },
   props: {
@@ -105,7 +139,11 @@ export default {
       this.$emit('close')
     },
     onModalClick (val) {
-      this.controllers.action = val
+      if (this.controllers.action === val) {
+        this.controllers.action = undefined
+      } else {
+        this.controllers.action = val
+      }
     },
     onDeleteClick () {
       this.$emit('delete')
@@ -129,21 +167,46 @@ export default {
 }
 </script>
 
-<style scoped>
-  .update-gif-overlay {
-    background-color: #00000099;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    left: 0;
+<style scoped lang="less">
+
+  @import url('@/assets/styles/colors.less');
+
+  .update-gif-modal {
+    border-radius: inherit;
+    max-width: 50vw;
   }
-  .overlay-card {
-      min-width: 25vw;
+  @media all and (max-width: 400) {
+    .update-gif-modal {
+      max-width: 100vw;
     }
+  }
+  @media all and (max-width: 600) {
+    .update-gif-modal {
+      max-width: 90vw;
+    }
+  }
+  @media all and (max-width: 900) {
+    .update-gif-modal {
+      max-width: 75vw;
+    }
+  }
+  @media all and (max-width: 1200) {
+    .update-gif-modal {
+      max-width: 50vw;
+    }
+  }
+  @media all and (min-width: 1900) {
+    .update-gif-modal {
+      max-width: 25vw;
+    }
+  }
   .actions-text {
     font-size: 1.25em;
     line-height: 1.5em;
     color: white;
     margin-top: 16px;
+  }
+  .actions-inactive {
+    line-height: 0;
   }
 </style>
